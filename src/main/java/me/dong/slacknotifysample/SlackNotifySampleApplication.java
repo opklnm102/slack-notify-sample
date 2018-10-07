0e2dc1c6eacc782ad0aae4f8b41052dbf65814b2
Dong Hee Kim <opklnm102@gmail.com>
2018-10-07 17:09:57 +0900

    initial commit

---
+++ src/main/java/me/dong/slacknotifysample/SlackNotifySampleApplication.java
@@ -0,0 +1,12 @@
+package me.dong.slacknotifysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackNotifySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackNotifySampleApplication.class, args);
+    }
+}
